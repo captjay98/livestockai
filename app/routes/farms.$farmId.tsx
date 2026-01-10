e3f4c6b56c17b128f2580afd3c2d7b3b9e248a7e
Jamal Ibrahim Umar <captjay98@gmail.com>
2026-01-10 22:13:01 +0100

    chore(routes): remove old unprotected route files
    Routes moved to _auth prefix for authentication protection

--- app/routes/farms.$farmId.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/farms/$farmId')({
-  component: FarmLayout,
-})
-
-function FarmLayout() {
-  return <Outlet />
-}
--- app/routes/farms.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/farms')({
-  component: FarmsLayout,
-})
-
-function FarmsLayout() {
-  return <Outlet />
-}
