c321ef339ee560cbacf4953e99a5946b36b53da9
Jamal Ibrahim Umar <captjay98@gmail.com>
2026-01-13 15:55:11 +0100

    refactor(routes): remove old flat route files
    Remove flat route files that were converted to directory structure.
    Old: app/routes/_auth.batches.tsx
    New: app/routes/_auth/batches/index.tsx

--- app/routes/_auth.farms.$farmId.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_auth/farms/$farmId')({
-  component: FarmLayout,
-})
-
-function FarmLayout() {
-  return <Outlet />
-}
